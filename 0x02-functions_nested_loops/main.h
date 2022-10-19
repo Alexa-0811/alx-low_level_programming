1cb492ab860da459d2059dc2c7ce47c2c443dd25
Alexa-0811 <113188613+Alexa-0811@users.noreply.github.com>
2022-10-19 16:05:24 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H
+##ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,7 +13,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(?int n);
-int _putchar(char c)
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
